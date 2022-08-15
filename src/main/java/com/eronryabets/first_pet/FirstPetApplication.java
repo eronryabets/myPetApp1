ecdeabe9587fbd82800db40ad2d008de0da659de
Ihor Ryabets <eronryabets@gmail.com>
2022-08-15 15:14:54 +0300

    start

---
+++ src/main/java/com/eronryabets/first_pet/FirstPetApplication.java
@@ -0,0 +1,13 @@
+package com.eronryabets.first_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstPetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eronryabets/first_pet/FirstPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eronryabets.first_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
