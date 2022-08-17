7f92cec4a6a9c70ba794522d1a7531f0bdf1c414
Ihor Ryabets <eronryabets@gmail.com>
2022-08-17 18:46:21 +0300

    update Security (permit only Admin to UserController)

--- src/main/java/com/eronryabets/first_pet/controller/UserController.java
+++ src/main/java/com/eronryabets/first_pet/controller/UserController.java
@@ -4,6 +4,7 @@ import com.eronryabets.first_pet.entity.Role;
 import com.eronryabets.first_pet.entity.User;
 import com.eronryabets.first_pet.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
