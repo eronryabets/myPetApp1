8f69106a942f02334cbd5c4a11a25da9b24bee93
Ihor Ryabets <eronryabets@gmail.com>
2022-08-18 18:22:09 +0300

    update Service

--- src/main/java/com/eronryabets/first_pet/controller/UserController.java
+++ src/main/java/com/eronryabets/first_pet/controller/UserController.java
@@ -3,16 +3,16 @@ package com.eronryabets.first_pet.controller;
 import com.eronryabets.first_pet.entity.Role;
 import com.eronryabets.first_pet.entity.User;
 import com.eronryabets.first_pet.repository.UserRepository;
+import com.eronryabets.first_pet.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+
 
 @Controller
 @RequestMapping("/user")
@@ -20,6 +20,9 @@ import java.util.stream.Collectors;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping
     public String userList(Model model) {
@@ -43,23 +46,7 @@ public class UserController {
             @RequestParam Map<String, String> form,
             @RequestParam("userId") User user
     ){
-        user.setUsername(username);
-        user.setName(name);
-        user.setSurname(surname);
-        user.setPassword(password);
-
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-        for(String key : form.keySet()){
-            if(roles.contains(key)){
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-
-        userRepository.save(user);
+        userService.userSave(user,username,name,surname,password,form);
         return "redirect:/user";
     }
 
--- src/main/java/com/eronryabets/first_pet/service/UserService.java
+++ src/main/java/com/eronryabets/first_pet/service/UserService.java
@@ -1,11 +1,18 @@
 package com.eronryabets.first_pet.service;
 
+import com.eronryabets.first_pet.entity.Role;
+import com.eronryabets.first_pet.entity.User;
 import com.eronryabets.first_pet.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -17,4 +24,26 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
     }
+
+    public void userSave(User user, String username, String name, String surname,
+                         String password, Map<String, String> form) {
+
+        user.setUsername(username);
+        user.setName(name);
+        user.setSurname(surname);
+        user.setPassword(password);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+    }
+
 }
