5698d58cbc30d10d88619bc3549193fe28297020
Ihor Ryabets <eronryabets@gmail.com>
2022-08-23 14:49:20 +0300

    add finance table for wallets

---
+++ src/main/java/com/eronryabets/first_pet/repository/FinanceRepository.java
@@ -0,0 +1,7 @@
+package com.eronryabets.first_pet.repository;
+
+import com.eronryabets.first_pet.entity.Finance;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FinanceRepository extends CrudRepository<Finance, Integer> {
+}
