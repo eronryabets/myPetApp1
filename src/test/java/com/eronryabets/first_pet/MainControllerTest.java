d39ea2aa626e5183301a44e79f2245cbe09a286a
Ihor Ryabets <eronryabets@gmail.com>
2022-09-13 18:27:33 +0300

    add Integration Testing

--- src/test/java/com/eronryabets/first_pet/MainControllerTest.java
+++ src/test/java/com/eronryabets/first_pet/MainControllerTest.java
@@ -1,4 +1,55 @@
 package com.eronryabets.first_pet;
 
+import com.eronryabets.first_pet.controller.MainController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+//@TestPropertySource("/application-test.properties")
+//@Sql(value = {"/create-user-before.sql", "/messages-list-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+//@Sql(value = {"/messages-list-after.sql", "/create-user-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@WithUserDetails(value = "admin")
 public class MainControllerTest {
+
+    @Autowired
+    private MainController controller;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void mainPageTest() throws Exception {
+        this.mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(authenticated())
+                .andExpect(xpath("//*[@id='navbarSupportedContent']/div").string("admin"));
+    }
+
+    @Test
+    public void messageListTest() throws Exception {
+        this.mockMvc.perform(get("/messages"))
+                .andDo(print())
+                .andExpect(authenticated())
+                .andExpect(xpath("//*[@id='message-list']/div").nodeCount(6));
+    }
+
+
 }
