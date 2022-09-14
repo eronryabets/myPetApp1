ceb4ffe8e428b3698075643f60e935cc9946a256
Ihor Ryabets <eronryabets@gmail.com>
2022-09-14 18:18:06 +0300

    unit testing (mock object)

---
+++ src/test/java/com/eronryabets/first_pet/UserServiceTest.java
@@ -0,0 +1,68 @@
+package com.eronryabets.first_pet;
+
+import com.eronryabets.first_pet.entity.Role;
+import com.eronryabets.first_pet.entity.User;
+import com.eronryabets.first_pet.repository.UserRepository;
+import com.eronryabets.first_pet.service.UserService;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+
+import java.util.Collections;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void addUser() {
+        User user = new User();
+
+        boolean isUserCreated = userService.addUser(user);
+
+        Assert.assertTrue(isUserCreated);
+        Assert.assertTrue(CoreMatchers.is(user.getRoles())
+                .matches(Collections.singleton(Role.USER)));
+        Mockito.verify(userRepository, Mockito.times(1))
+                .save(user);
+    }
+
+    @Test
+    public void addUserFailTest() {
+        User user = new User();
+        user.setUsername("John");
+
+        Mockito.doReturn(new User())
+                .when(userRepository)
+                .findByUsername("John");
+
+        boolean isUserCreated = userService.addUser(user);
+
+        Assert.assertFalse(isUserCreated);
+        Mockito.verify(userRepository, Mockito.times(0))
+                .save(ArgumentMatchers.any(User.class));
+    }
+
+
+
+
+
+
+}
